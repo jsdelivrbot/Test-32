99fa280b38e12a450a96a5433dea947f96f1d801
Stephen Mayeux <stephenmayeux@gmail.com>
2017-10-17 19:09:23 -0500

    remove stuff. clean up app

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-//var ReactDOM = require('react-dom');
-
 import axios from 'axios';
 
 import CamperList from './camper_list.js';
 import CamperListItem from './camper_list_item.js';
 
-export default class FetchComp extends React.Component {
+export default class FetchComp extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
@@ -17,57 +15,47 @@ export default class FetchComp extends React.Component {
 			currentDisplay: 'recentCampers'
 		}
 	}
-	
+
   componentDidMount(){
-	  
     axios.all([this.fetchRecentCampers(), this.fetchTopCampers()])
       .then(axios.spread((recentCampers, allTimeCampers) => {
-			console.log(recentCampers.data);
-			
 			this.setState({
-            recentCampers: recentCampers.data,
-            allTimeCampers: allTimeCampers.data
-          });  
-      }));
+        recentCampers: recentCampers.data,
+        allTimeCampers: allTimeCampers.data
+      });
+    }));
   }
-  
+
     fetchRecentCampers(){
       return axios.get('https://fcctop100.herokuapp.com/api/fccusers/top/recent');
     }
-    
+
     fetchTopCampers(){
       return axios.get('https://fcctop100.herokuapp.com/api/fccusers/top/alltime');
     }
-	
-	changeDisplay(list){
-		this.setState({ list });
-	}
+
+		changeDisplay(list){
+			this.setState({ list });
+		}
 
     render() {
-
-		console.log(this.state);
-	
       return(
-	  
         <div>
-            <h2>  </h2>
-            <button onClick={() => this.changeDisplay('recentCampers')} className="btn btn-primary"> Recent Campers </button>
-            <button onClick={() => this.changeDisplay('allTimeCampers')} className="btn btn-primary"> All Time Campers </button>
-			
-			<div>
-			  <CamperList campers={this.state.recentCampers} > </CamperList>
-			</div>
+	        <h2>  </h2>
+	        <button
+						onClick={() => this.changeDisplay('recentCampers')}
+						className="btn btn-primary"
+					>
+						Recent Campers
+					</button>
+	        <button
+						onClick={() => this.changeDisplay('allTimeCampers')}
+						className="btn btn-primary"
+					>
+						All Time Campers
+					</button>
+			  	<CamperList campers={this.state.recentCampers} />
         </div>
       );
     }
 }
-
-
-
-
-
-
-ReactDOM.render(
-  React.createElement(FetchComp, null, null),
-  document.getElementById('root')
-);
